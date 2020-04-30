7fc29ef7945df4da6e9ffdc6afe0f7f6a526c264
francistse-realm <wtse.ucsd@gmail.com>
2020-04-29 21:02:30 -0700

    Extracted modal to separate functional component. Added Modal to render media.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {
   Button,
+  Dimensions,
+  Modal,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -11,10 +13,12 @@ import { Video } from "expo-av";
 
 export default function App() {
   const [awws, setAwws] = React.useState([]);
-  const [timer, setTimer] = React.useState(0);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+  // start/pause
   const [isTimerActive, setIsTimerActive] = React.useState(false);
-
-  const videoRef = React.useRef(null);
+  // timer's completion status
+  const [isTimerDone, setIsTimerDone] = React.useState(false);
+  const [timer, setTimer] = React.useState(0);
 
   const calculateTimeRemaining = (time) => {
     const mins = Math.floor(time / 60);
@@ -23,6 +27,11 @@ export default function App() {
     return `${mins >= 10 ? mins : `0${mins}`}:${
       seconds.toString().length > 1 ? seconds : `0${seconds}`
     }`;
+  };
+
+  const setTime = (time) => {
+    setTimer(time * 60);
+    setIsTimerDone(false);
   };
 
   React.useEffect(() => {
@@ -39,7 +48,9 @@ export default function App() {
 
   React.useEffect(() => {
     if (timer <= 0) {
+      setIsModalVisible(true);
       setIsTimerActive(false);
+      setIsTimerDone(true);
     }
   }, [timer]);
 
@@ -55,76 +66,101 @@ export default function App() {
     getData();
   }, []);
 
-  const randomImage = Math.floor(Math.random() * awws.length);
-
   // share image to friend, show when their timer is complete
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 64 }}>Timer</Text>
-
-      <View>
-        <TouchableOpacity
-          onPress={() => setTimer(20 * 60)}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>20 minutes</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => setTimer(25 * 60)}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>25 minutes</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => setTimer(30 * 60)}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>30 minutes</Text>
-        </TouchableOpacity>
-      </View>
+      <Text style={{ flex: 1, fontSize: 64 }}>Timer</Text>
 
       {timer > 0 ? (
-        <View>
-          <Text>{calculateTimeRemaining(timer)}</Text>
+        <View
+          style={{
+            alignItems: "center",
+            flex: 5,
+            width: "100%",
+          }}
+        >
+          <Text style={{ fontSize: 40 }}>{calculateTimeRemaining(timer)}</Text>
           <TouchableOpacity onPress={() => setIsTimerActive((state) => !state)}>
-            <Text>{isTimerActive ? "Pause" : "Start"}</Text>
+            <Text style={{ fontSize: 40 }}>
+              {isTimerActive ? "Pause" : "Start"}
+            </Text>
           </TouchableOpacity>
         </View>
       ) : (
-        awws.length > 0 && (
-          <View style={{ width: "100%" }}>
-            {awws[randomImage].url.endsWith(".jpg") ? (
-              <Image
-                source={{
-                  uri: awws[randomImage].url,
-                }}
-                style={{ height: 200 }}
-              />
-            ) : (
-              <Video
-                isLooping
-                onLoad={() => videoRef.current.presentFullscreenPlayer()}
-                ref={videoRef}
-                resizeMode="contain"
-                shouldPlay
-                source={{
-                  uri: awws[randomImage].secure_media?.reddit_video?.dash_url,
-                }}
-                style={{ flex: 1 }}
-              />
-            )}
-            <Text style={{ textAlign: "center" }}>
-              {awws[randomImage].title}
-            </Text>
-          </View>
-        )
+        <View
+          style={{ flex: 4, width: "100%", borderColor: "red", borderWidth: 1 }}
+        >
+          <TouchableOpacity onPress={() => setTime(10)} style={styles.button}>
+            <Text style={styles.buttonText}>10 minutes</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setTime(15)} style={styles.button}>
+            <Text style={styles.buttonText}>15 minutes</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setTime(30)} style={styles.button}>
+            <Text style={styles.buttonText}>30 minutes</Text>
+          </TouchableOpacity>
+
+          {isTimerDone && awws.length > 0 && (
+            <MediaModal
+              awws={awws}
+              isModalVisible={isModalVisible}
+              onClose={() => setIsModalVisible(false)}
+            />
+          )}
+        </View>
       )}
     </View>
   );
 }
+
+const MediaModal = ({ awws, isModalVisible, onClose }) => {
+  const videoRef = React.useRef(null);
+
+  const randomImage = Math.floor(Math.random() * awws.length);
+
+  return (
+    <Modal
+      statusBarTranslucent={false}
+      style={{ flex: 1, width: "100%" }}
+      visible={isModalVisible}
+    >
+      <View>
+        <TouchableOpacity onPress={onClose}>
+          <Text style={{ fontSize: 40 }}>X</Text>
+        </TouchableOpacity>
+        {awws[randomImage].url.endsWith(".jpg") ? (
+          <Image
+            resizeMode="contain"
+            source={{
+              uri: awws[randomImage].url,
+            }}
+            style={{
+              width: Dimensions.get("window").width,
+              height: Dimensions.get("window").height,
+            }}
+          />
+        ) : (
+          <Video
+            isLooping
+            onLoad={() => videoRef.current.presentFullscreenPlayer()}
+            ref={videoRef}
+            resizeMode="contain"
+            shouldPlay
+            source={{
+              uri: awws[randomImage].secure_media?.reddit_video?.dash_url,
+            }}
+            style={{ flex: 1 }}
+            useNativeControls
+          />
+        )}
+        <Text style={{ textAlign: "center" }}>{awws[randomImage].title}</Text>
+      </View>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
@@ -137,11 +173,13 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 24,
+    textAlign: "center",
   },
   container: {
-    flex: 1,
-    backgroundColor: "white",
     alignItems: "center",
+    backgroundColor: "white",
+    flex: 1,
     justifyContent: "center",
+    paddingVertical: 12,
   },
 });
