64e7c8c0b228ea3b0d2a4cfe07bd6cb86b58b2e3
David Drabik <david@daviddrabik.com>
2020-05-05 20:04:00 -0700

    move prize modal to new file
    Co-authored-by: Francis Tse <wtse.ucsd@gmail.com>

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 } from "react-native";
 import { Video } from "expo-av";
 import * as SecureStore from "expo-secure-store";
+import PrizeModal from "./PrizeModal";
 
 const ACTION_TYPES = {
   RESET: "RESET",
@@ -237,7 +238,7 @@ export default function App() {
       ) : (
         <View style={{ flex: 4, width: "100%" }}>
           {isTimerDone && isModalVisible && awws.length > 0 && (
-            <MediaModal
+            <PrizeModal
               aww={awws[randomImage]}
               friends={currentUser.friends}
               onClose={() => dispatch({ type: ACTION_TYPES.RESET })}
@@ -301,115 +302,6 @@ const TimerView = ({ isTimerActive, dispatch, timer }) => {
         </Text>
       </TouchableOpacity>
     </View>
-  );
-};
-
-const MediaModal = ({ aww, friends, onClose }) => {
-  const [isSharing, setIsSharing] = React.useState(false);
-  const [selectedFriends, setSelectedFriends] = React.useState([]);
-  const videoRef = React.useRef(null);
-
-  // moved randomImage out because tapping button causes rerender
-
-  const shareToFriends = async () => {
-    try {
-      for (let friend of selectedFriends) {
-        const data = [
-          aww.is_video ? aww.secure_media?.reddit_video?.dash_url : aww.url,
-          // following structure will throw an error
-          // {
-          //   [aww.id]: aww.is_video
-          //     ? aww.secure_media?.reddit_video?.dash_url
-          //     : aww.url,
-          // },
-        ];
-
-        let res = await fetch(
-          `https://awwtimer.firebaseio.com/users/${friend}/prizes.json`,
-          {
-            body: JSON.stringify(data),
-            headers: {
-              "Content-Type": "application/json",
-            },
-            // dont use post or put
-            // post, firebase will auto-generate a key/id and use that as the new user object's key, too chaotic
-            // put, replaces all users (effectively wiping all users)
-            method: "patch",
-            mode: "cors",
-          }
-        );
-        // res = await res.json();
-        // console.log("After sharing", res);
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
-
-  return (
-    <Modal
-      statusBarTranslucent={false}
-      style={{ flex: 1, width: "100%" }}
-      visible={true}
-    >
-      <View style={{ alignItems: "center", flex: 1, paddingVertical: 8 }}>
-        <TouchableOpacity
-          onPress={onClose}
-          style={{ alignSelf: "flex-start", margin: 8 }}
-        >
-          <Text style={{ fontSize: 30 }}>X</Text>
-        </TouchableOpacity>
-
-        <Text style={{ textAlign: "center" }}>{aww.title}</Text>
-
-        {aww.url.endsWith(".jpg") ? (
-          <Image
-            resizeMode="contain"
-            source={{
-              uri: aww.url,
-            }}
-            style={{
-              width: Dimensions.get("window").width * 0.7,
-              height: Dimensions.get("window").height * 0.7,
-            }}
-          />
-        ) : (
-          <>
-            {/* poster image ? */}
-            <Video
-              isLooping
-              onLoad={() => videoRef.current.presentFullscreenPlayer()}
-              ref={videoRef}
-              resizeMode="contain"
-              shouldPlay
-              source={{
-                uri: aww.secure_media?.reddit_video?.dash_url,
-              }}
-              style={{ flex: 1 }}
-              useNativeControls
-            />
-          </>
-        )}
-
-        {isSharing ? (
-          <FriendsList
-            selectedFriends={selectedFriends}
-            setSelectedFriends={setSelectedFriends}
-            friends={friends}
-            shareToFriends={shareToFriends}
-          />
-        ) : (
-          <TouchableOpacity
-            onPress={() => setIsSharing(true)}
-            style={styles.button}
-          >
-            <Text style={{ color: "white", fontSize: 18 }}>
-              Share ( because you care :) )
-            </Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    </Modal>
   );
 };
 
