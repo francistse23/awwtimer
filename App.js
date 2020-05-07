60a813d1c745eca0e6f838499545f793ac161ee0
David Drabik <david@daviddrabik.com>
2020-05-06 19:27:16 -0700

    hide prize btn when sharing

--- App.js
+++ App.js
@@ -217,7 +217,7 @@ export default function App() {
           />
         )}
 
-        {isTimerDone && (
+        {isTimerDone && !isSharing && (
           <TouchableOpacity
             style={styles.button}
             onPress={() =>
